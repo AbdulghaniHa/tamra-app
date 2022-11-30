2fd0aa2cb889ee2837895b65b7676555e27d620c
AbdulghaniHarran <abdulghani@rafraf.com>
2022-12-01 02:26:51 +0300

    [Fix] sma object props

--- src/component/calculation/TA/getSMA.ts
+++ src/component/calculation/TA/getSMA.ts
@@ -2,7 +2,8 @@ import { sma } from "indicatorts"
 export const getSMA = (data: any, range: number) => {
   const closeArray = data.map((d: any) => Number(d.close));
   const smaArray = sma(range, closeArray);
-  data = data.map((d: any, i: any) => ({ ...d, sma: smaArray[i] }));
+  data = data.map((d: any, i: any) => ({ ...d, sma: smaArray[i] })).slice(range).filter((d: any) => d.sma)
+  .map((d: any) => ({ time: d.time, value: d.sma }));
 
-  return data.slice(range)
+  return data
 };
