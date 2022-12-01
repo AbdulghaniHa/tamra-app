0ba51c5177f907e09aa46c1f07366eecb8ca449b
AbdulghaniHarran <abdulghani@rafraf.com>
2022-12-01 15:56:06 +0300

    custom error logs

--- src/component/api/getData.ts
+++ src/component/api/getData.ts
@@ -9,21 +9,25 @@ interface Props {
 
 export async function getData(props: Props) {
 
-    // Ticker will be capalized: goog -> GOOG
     const res = await fetch(`/${props.ticker}?period1=${props.from_timestamp}&period2=${props.to_timestamp}&interval=${props.interval}&events=history&crumb=5YTX%2FgVGBmg`)
 
     // Check if status 200
-    console.log(res)
     if (res.status === 200) {
         let text_data = await res.text()
         text_data = text_data.replace("Date,Open,High,Low,Close,Adj Close,Volume", "time,open,high,low,close,Adj Close,Volume")
         const json_data = csv2json(text_data)
-        return json_data
+        return {
+            "data": json_data,
+            "error": false
+        }
     }
     else {
         // Check if status != 200
         // return not found
-        return "Error"
+        return {
+            "data": [],
+            "error": true
+        }
     }
 
 }
